870fc05bbac7e203dfc67d47d1045ebc5085875b
popstr-kun <hw38752920@gmail.com>
2022-11-09 21:33:49 +0800

    ver finish

--- app/src/main/java/com/example/andorid_homework_kotlin2_2/MainActivity.kt
+++ app/src/main/java/com/example/andorid_homework_kotlin2_2/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.andorid_homework_kotlin2_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.example.andorid_homework_kotlin2_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +17,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         //setContentView(R.layout.activity_main)
 
-        
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        binding.viewPager.adapter = adapter
     }
+}
+
+class ViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm){
+    override fun getItem(position : Int) = when(position){
+        0    -> FirstFragment()
+        1    -> SecondFragment()
+        else -> ThirdFragment()
+    }
+
+    override fun getCount() = 3
 }
